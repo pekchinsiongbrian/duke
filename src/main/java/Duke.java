2a176a0a6ac525a6dbed15a3567f8c41d1fb4dc4
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-25 13:21:40 +0800

    Added Level-2 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,35 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String startHLine = "\t____________________________________________________________";
-        String endHLine = "\n\t____________________________________________________________\n";
-        System.out.println(startHLine + "\n\tHello! I'm Duke\n\tWhat can I do for you?" + endHLine);
         Scanner sc = new Scanner(System.in);
 
+        String horizontalLine = "\t____________________________________________________________";
+        System.out.println(horizontalLine + "\n\tHello! I'm Duke\n\tWhat can I do for you?\n" + horizontalLine);
+
+        String[] list = new String[100];
+        int indexToAdd = 0;
+
         while (true) {
+            System.out.println();
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
-                System.out.println(startHLine + "\n\tBye. Hope to see you again soon!" + endHLine);
+                System.out.println(horizontalLine + "\n\tBye. Hope to see you again soon!\n" + horizontalLine);
+                sc.close();
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < list.length; i++) {
+                    if (list[i] == null) {
+                        break;
+                    } else {
+                        System.out.println("\t" + (i + 1) + ". " + list[i]);
+                    }
+                }
+                System.out.println(horizontalLine);
             } else {
-                System.out.println(startHLine + "\n\t" + userInput + endHLine);
+                list[indexToAdd] = userInput;
+                indexToAdd += 1;
+                System.out.println(horizontalLine + "\n\tadded: " + userInput + "\n" + horizontalLine);
             }
         }
     }
