f1c1335b98c7be239296e5de6ef010cb018dc0ec
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-25 21:53:51 +0800

    Added Level-4 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private static final String HORIZONTAL_LINE = "\t____________________________________________________________";
@@ -13,13 +15,14 @@ public class Duke {
         while (true) {
             System.out.println();
             String userInput = sc.nextLine();
+            String firstWord = userInput.split(" ")[0];
             if (userInput.equals("bye")) {
                 String toPrint = "Bye. Hope to see you again soon!";
                 textFormatAndPrint(toPrint);
                 sc.close();
                 break;
             } else if (userInput.equals("list")) {
-                System.out.println(HORIZONTAL_LINE);
+                System.out.println(HORIZONTAL_LINE + "\n\tHere are the tasks in your list:");
                 for (int i = 0; i < list.length; i++) {
                     if (list[i] == null) {
                         break;
@@ -28,9 +31,9 @@ public class Duke {
                     }
                 }
                 System.out.println(HORIZONTAL_LINE);
-            } else if (userInput.split(" ")[0].equals("done")){
+            } else if (firstWord.equals("done")){
                 if (userInput.split(" ").length != 2) {
-                    list[indexToAdd] = createNewTask(userInput);
+                    list[indexToAdd] = createNewTask(userInput,indexToAdd);
                     indexToAdd += 1;
                 } else {
                     try {
@@ -43,20 +46,37 @@ public class Duke {
                             textFormatAndPrint(toPrint);
                         }
                     } catch (Exception e) {
-                        list[indexToAdd] = createNewTask(userInput);
+                        list[indexToAdd] = createNewTask(userInput, indexToAdd);
                         indexToAdd += 1;
                     }
                 }
+            } else if ((firstWord.equals("todo") || firstWord.equals("deadline")) || firstWord.equals("event")) {
+                list[indexToAdd] = createNewTask(userInput, indexToAdd);
+                indexToAdd += 1;
             } else {
-                list[indexToAdd] = createNewTask(userInput);
+                list[indexToAdd] = createNewTask(userInput, indexToAdd);
                 indexToAdd += 1;
             }
         }
     }
 
-    public static Task createNewTask(String userInput) {
-        Task t = new Task(userInput);
-        String toPrint = "added: " + userInput;
+    public static Task createNewTask(String userInput, int numOfTasks) {
+        Task t;
+        String taskType = userInput.split(" ")[0];
+        String description = userInput.substring(taskType.length() + 1);
+        if (taskType.equals("todo")) {
+            t = new Todo(description);
+        } else if (taskType.equals("deadline")) {
+            String by = userInput.substring(userInput.indexOf("/by") + 4);
+            t = new Deadline(description.substring(0, description.indexOf("/by") - 1), by);
+        } else if (taskType.equals("event")) {
+            String at = userInput.substring(userInput.indexOf("/at") + 4);
+            t = new Event(description.substring(0, description.indexOf("/at") - 1), at);
+        } else {
+            t = new Task("ERROR");
+        }
+        String toPrint = "Got it. I've added this task:\n\t\t" + t.toString() + "\n\tNow you have " + (numOfTasks + 1) +
+                " tasks in the list.";
         textFormatAndPrint(toPrint);
         return t;
     }
