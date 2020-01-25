81a6503a453811494e2a52d0b98ebaf033ae6a2b
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-25 13:19:42 +0800

    Added Level-1 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String startHLine = "\t____________________________________________________________";
+        String endHLine = "\n\t____________________________________________________________\n";
+        System.out.println(startHLine + "\n\tHello! I'm Duke\n\tWhat can I do for you?" + endHLine);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(startHLine + "\n\tBye. Hope to see you again soon!" + endHLine);
+                break;
+            } else {
+                System.out.println(startHLine + "\n\t" + userInput + endHLine);
+            }
+        }
     }
 }
