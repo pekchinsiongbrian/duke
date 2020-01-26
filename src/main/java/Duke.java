e8439fc5d7ce3dbe486af639776c228a648895d7
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-26 13:56:02 +0800

    Added Level-6 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,6 @@ public class Duke {
         textFormatAndPrint("Hello! I'm Duke\n\tWhat can I do for you?");
 
         ArrayList<Task> list = new ArrayList<Task>();
-        int indexToAdd = 0;
         boolean continueLoop = true;
 
         while (continueLoop) {
@@ -25,14 +24,15 @@ public class Duke {
                 switch(firstWord) {
                     case "TODO":
                         if (userInputSplit.length == 1) {
-                            throw new DukeException("The description of a TODO cannot be empty.");
+                            throw new DukeException("Use: todo <description>");
                         } else {
-                            list.add(indexToAdd, createNewTask(userInput, indexToAdd));
-                            indexToAdd += 1;
+                            list.add(createNewTask(userInput, list));
                             break;
                         }
                     case "DEADLINE":
-                        if (!userInput.contains("/by")) {
+                        if (userInputSplit.length == 1) {
+                            throw new DukeException("Use: deadline <task> /by <deadline>");
+                        } else if (!userInput.contains("/by")) {
                             throw new DukeException("Please use the '/by' keyword to specify a deadline." +
                                     "\nUse: deadline <task> /by <deadline>");
                         } else if (userInputSplit[1].equals("/by")) {
@@ -46,12 +46,13 @@ public class Duke {
                             throw new DukeException("Only one '/by' keyword can be used!" +
                                     "\nUse: deadline <task> /by <deadline>");
                         } else {
-                            list.add(indexToAdd, createNewTask(userInput, indexToAdd));
-                            indexToAdd += 1;
+                            list.add(createNewTask(userInput, list));
                             break;
                         }
                     case "EVENT":
-                        if (!userInput.contains("/at")) {
+                        if (userInputSplit.length == 1) {
+                            throw new DukeException("Use: deadline <task> /by <deadline>");
+                        } else if (!userInput.contains("/at")) {
                             throw new DukeException("Please use the '/at' keyword to specify the event day/date/time." +
                                     "\nUse: event <desc> /at <day/date/time>");
                         } else if (userInputSplit[1].equals("/at")) {
@@ -66,8 +67,7 @@ public class Duke {
                             throw new DukeException("Only one '/at' keyword can be used!" +
                                     "\nUse: event <desc> /at <day/date/time>");
                         } else {
-                            list.add(indexToAdd, createNewTask(userInput, indexToAdd));
-                            indexToAdd += 1;
+                            list.add(createNewTask(userInput, list));
                             break;
                         }
                     case "DONE":
@@ -84,7 +84,7 @@ public class Duke {
                                 } else {
                                     Task t = list.get(Integer.parseInt(userInputSplit[1]) - 1);
                                     t.markAsDone();
-                                    String toPrint = "Nice! I've marked this task as done:\n\t  " + t.toString();
+                                    String toPrint = "Nice! I've marked this task as done:\n\t\t" + t.toString();
                                     textFormatAndPrint(toPrint);
                                     break;
                                 }
@@ -93,13 +93,42 @@ public class Duke {
                                         "\nUse: done 2 (marks 2nd item on the list as done)");
                             }
                         }
+                    case "DELETE":
+                        if (userInputSplit.length != 2) {
+                            throw new DukeException("Enter index of item to delete it. " +
+                                    "Type 'list' to see all items.\nUse: delete 2 (deletes 2nd item off the list)");
+                        } else {
+                            try {
+                                if (Integer.parseInt(userInputSplit[1]) <= 0) {
+                                    throw new DukeException("Parameter must be a positive integer." +
+                                            "\nUse: delete 2 (deletes 2nd item off the list)");
+                                } else if (Integer.parseInt(userInputSplit[1]) > list.size()) {
+                                    throw new DukeException("Parameter exceeds number of items on the list!");
+                                } else {
+                                    Task t = list.get(Integer.parseInt(userInputSplit[1]) - 1);
+                                    String toPrint = "Noted. I've removed this task:\n\t\t" + t.toString() +
+                                            "\n\tNow you have " + (list.size() - 1) + " tasks in the list.";
+                                    textFormatAndPrint(toPrint);
+                                    list.remove(Integer.parseInt(userInputSplit[1]) - 1);
+                                    break;
+                                }
+                            } catch (NumberFormatException e) {
+                                throw new DukeException("Parameter must be a positive integer." +
+                                        "\nUse: delete 2 (deletes 2nd item off the list)");
+                            }
+                        }
                     case "LIST":
                         if (userInputSplit.length != 1) {
                             throw new DukeException("'List' has no parameters! Please try again.");
                         } else {
                             System.out.println(HORIZONTAL_LINE + "\n\tHere are the tasks in your list:");
-                            for (int i = 0; i < list.size(); i++) {
-                                System.out.println("\t" + (i + 1) + "." + list.get(i));
+                            if (list.isEmpty()) {
+                                System.out.println("\t\tYou have no tasks right now." +
+                                        "\n\t\tUse 'todo', 'deadline', or 'event' to add task!");
+                            } else {
+                                for (int i = 0; i < list.size(); i++) {
+                                    System.out.println("\t" + (i + 1) + "." + list.get(i));
+                                }
                             }
                             System.out.println(HORIZONTAL_LINE);
                             break;
@@ -119,7 +148,7 @@ public class Duke {
         }
     }
 
-    public static Task createNewTask(String userInput, int numOfTasks) {
+    public static Task createNewTask(String userInput, ArrayList<Task> list) {
         Task t;
         String taskType = userInput.split(" ")[0].toUpperCase();
         String description = userInput.substring(taskType.length() + 1);
@@ -141,7 +170,7 @@ public class Duke {
 
         assert t != null;
         String toPrint = "Got it. I've added this task:\n\t\t" + t.toString() + "\n\tNow you have " +
-                (numOfTasks + 1) + " tasks in the list.";
+                (list.size() + 1) + " tasks in the list.";
         textFormatAndPrint(toPrint);
         return t;
     }
