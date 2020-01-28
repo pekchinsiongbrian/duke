eca24ad3efecd90d88468bc0fcaec57a27bc0605
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-28 23:28:57 +0800

    Added Level-8 features

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,57 @@
-public class Deadline extends Task {
-    protected String by;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String by) {
+public class Deadline extends Task {
+    protected LocalDate byDate;
+    protected LocalTime byTime;
+
+    public Deadline(String description, LocalDate byDate) {
         super(description);
-        this.by = by;
+        this.byDate = byDate;
+    }
+
+    public Deadline(String description, LocalDate byDate, LocalTime byTime) {
+        super(description);
+        this.byDate = byDate;
+        this.byTime = byTime;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        if (this.byTime == null) {
+            return "[D]" + super.toString() + " (by: " +
+                    this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " +
+                    this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                    ", " + this.byTime.format(DateTimeFormatter.ofPattern("HH:mm")) + ")";
+        }
+    }
+
+    public String displayDeadline() {
+        try {
+            if (this.byTime == null) {
+                return "[D]" + super.toString() + " (by: " +
+                        this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ") - " +
+                        LocalDate.now().datesUntil(this.byDate).count() + " day(s) to go!";
+            } else {
+                return "[D]" + super.toString() + " (by: " +
+                        this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                        ", " + this.byTime.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                        LocalDate.now().datesUntil(this.byDate).count() + " day(s) to go!";
+            }
+        } catch (IllegalArgumentException iae) {
+            if (this.byTime == null) {
+                return "[D]" + super.toString() + " (by: " +
+                        this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ") - " +
+                        "it's " + this.byDate.datesUntil(LocalDate.now()).count() + " day(s) past the deadline!";
+            } else {
+                return "[D]" + super.toString() + " (by: " +
+                        this.byDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                        ", " + this.byTime.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                        "it's " + this.byDate.datesUntil(LocalDate.now()).count() + " day(s) past the deadline!";
+            }
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,88 @@
-public class Event extends Task {
-    protected String at;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String at) {
+public class Event extends Task {
+    protected LocalDate atDate;
+    protected LocalTime atTimeStart;
+    protected LocalTime atTimeEnd;
+
+    public Event(String description, LocalDate atDate) {
         super(description);
-        this.at = at;
+        this.atDate = atDate;
+    }
+
+    public Event(String description, LocalDate atDate, LocalTime atTimeStart) {
+        super(description);
+        this.atDate = atDate;
+        this.atTimeStart = atTimeStart;
+    }
+
+    public Event(String description, LocalDate atDate, LocalTime atTimeStart, LocalTime atTimeEnd) {
+        super(description);
+        this.atDate = atDate;
+        this.atTimeStart = atTimeStart;
+        this.atTimeEnd = atTimeEnd;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        if (this.atTimeStart == null) {
+            return "[E]" + super.toString() + " (at: " +
+                    this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ")";
+        } else {
+            if (this.atTimeEnd == null) {
+                return "[E]" + super.toString() + " (at: " +
+                        this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                        ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) + ")";
+            } else {
+                return "[E]" + super.toString() + " (at: " +
+                        this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                        ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) +
+                        " to " + this.atTimeEnd.format(DateTimeFormatter.ofPattern("HH:mm")) + ")";
+            }
+        }
+    }
+
+    public String displayEventTime() {
+        try {
+            if (this.atTimeStart == null) {
+                return "[D]" + super.toString() + " (by: " +
+                        this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ") - " +
+                        LocalDate.now().datesUntil(this.atDate).count() + " day(s) to go!";
+            } else {
+                if (this.atTimeEnd == null) {
+                    return "[D]" + super.toString() + " (by: " +
+                            this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                            ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                            LocalDate.now().datesUntil(this.atDate).count() + " day(s) to go!";
+                } else {
+                    return "[D]" + super.toString() + " (by: " +
+                            this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                            ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) +
+                            " to " + this.atTimeEnd.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                            LocalDate.now().datesUntil(this.atDate).count() + " day(s) to go!";
+                }
+            }
+        } catch (IllegalArgumentException iae) {
+            if (this.atTimeStart == null) {
+                return "[D]" + super.toString() + " (by: " +
+                        this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + ") - " +
+                        "it's " + this.atDate.datesUntil(LocalDate.now()).count() + " day(s) past the event!";
+            } else {
+                if (this.atTimeEnd == null) {
+                    return "[D]" + super.toString() + " (by: " +
+                            this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                            ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                            "it's " + this.atDate.datesUntil(LocalDate.now()).count() + " day(s) past the event!";
+                } else {
+                    return "[D]" + super.toString() + " (by: " +
+                            this.atDate.format(DateTimeFormatter.ofPattern("d MMM yyyy")) +
+                            ", " + this.atTimeStart.format(DateTimeFormatter.ofPattern("HH:mm")) +
+                            " to " + this.atTimeEnd.format(DateTimeFormatter.ofPattern("HH:mm")) + ") - " +
+                            "it's " + this.atDate.datesUntil(LocalDate.now()).count() + " day(s) past the event!";
+                }
+            }
+        }
     }
 }
