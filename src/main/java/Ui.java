a0be4038bf1fda522a44a005fa9fd02b8f79bf31
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-01-31 21:56:27 +0800

    Trivial deletion of newline

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -106,5 +106,4 @@ public class Ui {
     public void showBottomLine() {
         System.out.println("\t________________________________________________________________________________\n");
     }
-
 }
